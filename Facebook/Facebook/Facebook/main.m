d66a35fd115ba8065788bccec52243b5467f16ec
CCDM02 <ccdm02@iMac-de-CCDM02.local>
2014-08-15 11:41:37 -0600

    agregue facebook

---
+++ Facebook/Facebook/Facebook/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Facebook
+//
+//  Created by CCDM02 on 15/08/14.
+//  Copyright (c) 2014 CCDM02. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
