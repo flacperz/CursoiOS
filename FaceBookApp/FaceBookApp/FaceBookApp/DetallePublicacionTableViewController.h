607c0849e5caab66293dc33623c8cfbb0d8e8fb4
CCDM02 <ccdm02@iMac-de-CCDM02.local>
2014-08-16 14:00:41 -0600

    actualize 2:00

---
+++ FaceBookApp/FaceBookApp/FaceBookApp/DetallePublicacionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetallePublicacionTableViewController.h
+//  FaceBookApp
+//
+//  Created by CCDM02 on 16/08/14.
+//  Copyright (c) 2014 CCDM02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetallePublicacionTableViewController : UITableViewController
+
+@end
