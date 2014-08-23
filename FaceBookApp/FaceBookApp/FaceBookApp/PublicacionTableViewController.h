6f32ab5240013f701ea2661eece4f20188d703c2
CCDM02 <ccdm02@iMac-de-CCDM02.local>
2014-08-23 12:24:48 -0600

    algo

---
+++ FaceBookApp/FaceBookApp/FaceBookApp/PublicacionTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  PublicacionTableViewController.h
+//  FaceBookApp
+//
+//  Created by CCDM02 on 23/08/14.
+//  Copyright (c) 2014 CCDM02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PublicacionTableViewController : UITableViewController <
+    UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *txtEstado;
+
+@end
