411c2e7cc6d31efc75153ce458bdd32988d7915d
CCDM24 <LABORATORIOMAC@iMac-de-CCDM24.local>
2014-08-30 13:47:51 -0600

    sabado 30

---
+++ FaceBookApp/FaceBookApp/FaceBookApp/AmigoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AmigoViewController.h
+//  FaceBookApp
+//
+//  Created by CCDM24 on 30/08/14.
+//  Copyright (c) 2014 CCDM24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AmigoViewController : UICollectionViewController
+
+@end
---
+++ FaceBookApp/FaceBookApp/UbicacionViewController.m
@@ -0,0 +1,49 @@
+//
+//  UbicacionViewController.m
+//  FaceBookApp
+//
+//  Created by CCDM24 on 30/08/14.
+//  Copyright (c) 2014 CCDM24. All rights reserved.
+//
+
+#import "UbicacionViewController.h"
+
+@interface UbicacionViewController ()
+
+@end
+
+@implementation UbicacionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
